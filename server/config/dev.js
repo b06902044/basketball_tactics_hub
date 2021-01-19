6f5d87cf495d9cc144020f8343a2cd021e12dd00
yangchen <b06902044@ntu.edu.tw>
2021-01-19 20:34:49 +0800

    fix_dev

--- server/config/dev.js
+++ server/config/dev.js
@@ -1,3 +1,5 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://yang:basketball30@cluster0.6ryji.mongodb.net/TacticHub?retryWrites=true&w=majority'
+    mongoURI: ""
 }
+
+//mongodb+srv://yang:basketball30@cluster0.6ryji.mongodb.net/TacticHub?retryWrites=true&w=majority'
