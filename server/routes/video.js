836dadc8ed3e80d5792bb2d6547f0bbd7584925e
yangchen <b06902044@ntu.edu.tw>
2021-01-19 23:51:14 +0800

    log

--- server/routes/video.js
+++ server/routes/video.js
@@ -27,6 +27,8 @@ const upload = multer({ storage: storage }).single('file');
 router.post('/uploadfiles', (req, res) => {
     upload(req, res, err => {
         if(err){
+            console.log("res", res);
+            console.log("err" , err);
             return res.json({success: false, err});
         }
         return res.json({success: true, filePath: res.req.file.path, fileName: res.req.file.filename});
